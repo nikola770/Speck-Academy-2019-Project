0a882b21b0bcdcaa34eee037382c3393032b98d6
nikola770 <nikola.trivnaovic@gmail.com>
2019-04-18 22:58:32 +0200

    add another file for import data

---
+++ db/inser2.sql
@@ -0,0 +1,23 @@
+
+INSERT INTO users (
+    user_id,
+    first_name,
+    last_name,
+    email,
+    user_password
+    )
+VALUES (
+    '1a36cf4b-853d-41fa-a59f-49e96b843c85',
+    'Nikola',
+    'Trivanović',
+    'nikola.trivanovic@gmail.com',
+    'password'
+),
+
+VALUES (
+    'd1698335-4f6e-45e0-a676-6592efbfd1ec',
+    'ANdrija',
+    'Biškup',
+    'andrija.biskup@gmail.com',
+    'password'
+);
